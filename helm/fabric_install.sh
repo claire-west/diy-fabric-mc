8cb6c8ae7fa00d2d2090a1655d73c3db2fc33971
claire-west <bokthaku@gmail.com>
2022-03-24 10:55:24 -0400

    minor fixes

--- helm/fabric_install.sh
+++ helm/fabric_install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-. setup_env.sh
-helm upgrade --install minecraft itzg/minecraft --values fabric/values.yaml --create-namespace --namespace fabric --set minecraftServer.eula=true,minecraftServer.rcon.password=$RCON_PW,minecraftServer.whitelist=$ALLOWLIST,minecraftServer.ops=$OPS
+source .env
+helm upgrade --install minecraft itzg/minecraft --values fabric/values.yaml --create-namespace --namespace fabric --set minecraftServer.eula=true,minecraftServer.rcon.password=$RCON_PW,minecraftServer.whitelist="$ALLOWLIST",minecraftServer.ops="$OPS"
